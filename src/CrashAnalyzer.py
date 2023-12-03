abf86c17eae4fe41b4057a9ce25ac19aa5549582
Thilo Molitor <thilo@eightysoft.de>
2023-12-04 00:56:34 +0100

    Step 2.66: Make CrashAnalyzer startable again

--- src/CrashAnalyzer.py
+++ src/CrashAnalyzer.py
@@ -6,6 +6,7 @@ import signal
 import argparse
 from PyQt5 import QtWidgets
 from CrashAnalyzer.ui import MainWindow
+from shared.utils.paths import Paths
 
 def sigint_handler(sig, frame):
     logger.warning('Main thread got interrupted, shutting down...')
@@ -17,9 +18,20 @@ parser.add_argument("file", type=str, help="Directly load given file", nargs="?"
 parser.add_argument("--log", metavar='LOGLEVEL', help="Loglevel to log", default="INFO")
 args = parser.parse_args()
 
+Paths.set_personality(__file__)
+os.makedirs(Paths.user_data_dir(), exist_ok=True)
+os.makedirs(Paths.user_log_dir(), exist_ok=True)
+
 import json, logging, logging.config
-with open(os.path.join(os.path.dirname(sys.argv[0]), "conf", "logger.json"), 'r') as logging_configuration_file:
-    logger_config = json.load(logging_configuration_file)
+try:
+    with open(Paths.get_conf_filepath("logger.json"), 'r') as logging_configuration_file:
+        logger_config = json.load(logging_configuration_file)
+except:
+    print(Paths.get_default_conf_filepath("logger.json"))
+    with open(Paths.get_default_conf_filepath("logger.json"), 'rb') as fp:
+        logger_config = json.load(fp)
+    with open(Paths.get_conf_filepath("logger.json"), 'w+') as fp:
+        json.dump(logger_config, fp)
 logger_config["handlers"]["stderr"]["level"] = args.log
 logging.config.dictConfig(logger_config)
 logger = logging.getLogger(__name__)
--- src/shared/utils/paths.py
+++ src/shared/utils/paths.py
@@ -1,30 +1,39 @@
 import sys
 import os
 import platformdirs
-from utils.constants import PLATFORM_ARGS
 
 import logging
 logger = logging.getLogger(__name__)
 
-def get_basedir_path():
-    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-        return sys._MEIPASS
-    return os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+class Paths:
+    def set_personality(file):
+        basename = os.path.splitext(os.path.basename(file))[0]
+        Paths.PLATFORM_ARGS = (basename, "monal-im")
+        Paths.BASEDIR = os.path.join(os.path.dirname(os.path.abspath(file)), basename)
+    
+    def get_basedir_path():
+        if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+            return sys._MEIPASS
+        return os.path.abspath(Paths.BASEDIR)
 
-def get_ui_filepath(filename):
-    return os.path.abspath(os.path.join(get_basedir_path(), "ui", filename))
+    def get_ui_filepath(filename):
+        return os.path.abspath(os.path.join(Paths.get_basedir_path(), "ui", filename))
 
-def get_art_filepath(filename):
-    return os.path.abspath(os.path.join(get_basedir_path(), "data", "art", filename))
+    def get_art_filepath(filename):
+        return os.path.abspath(os.path.join(Paths.get_basedir_path(), "data", "art", filename))
 
-def get_default_conf_filepath(filename):
-    return os.path.abspath(os.path.join(get_basedir_path(), "data", "conf", filename))
+    def get_default_conf_filepath(filename):
+        return os.path.abspath(os.path.join(Paths.get_basedir_path(), "data", "conf", filename))
 
-def get_conf_filepath(filename):
-    return os.path.join(user_data_dir(), filename)
+    def get_conf_filepath(filename):
+        return os.path.abspath(os.path.join(Paths.user_data_dir(), filename))
 
-def user_data_dir():
-    return platformdirs.user_data_dir(*PLATFORM_ARGS, roaming=True)
+    def user_data_dir():
+        return os.path.abspath(platformdirs.user_data_dir(*Paths.PLATFORM_ARGS, roaming=True))
 
-def user_log_dir():
-    return platformdirs.user_log_dir(*PLATFORM_ARGS)
+    def user_log_dir():
+        return os.path.abspath(platformdirs.user_log_dir(*Paths.PLATFORM_ARGS))
+
+# dummy defaults to be overwritten by main.py
+Paths.PLATFORM_ARGS = ()
+Paths.BASEDIR = "."
