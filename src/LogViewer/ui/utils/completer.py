15449998a1f543049bf76ff24b85f3c323a74f7e
Sobottasgithub <Sobotta2008@outlook.de>
2024-03-24 21:46:53 +0100

    [MLV] Honor casing in filter and search inputs, fixes #11
    setCaseSensitivity to allow the correct casing,
    see here for reference: https://codebrowser.dev/qt5/qtbase/src/widgets/widgets/qcombobox.cpp.html#1322

--- src/LogViewer/ui/utils/completer.py
+++ src/LogViewer/ui/utils/completer.py
@@ -1,10 +1,12 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 #custom completer, see here for reference: https://stackoverflow.com/a/36296644
 class Completer(QtWidgets.QCompleter):
     # Add texts instead of replace
     def pathFromIndex(self, index):
         path = QtWidgets.QCompleter.pathFromIndex(self, index)
+        self.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
+        self.setCompletionMode(Completer.PopupCompletion)
         current_parts = str(self.widget().lineEdit().text()).split(" ")
         if len(current_parts) > 1:
             path = '%s %s' % (" ".join(current_parts[:-1]), path)   # replace last part with selected completion
