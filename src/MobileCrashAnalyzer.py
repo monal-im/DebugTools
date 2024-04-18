6afb922abc0e08fcccd0f8b3f972ef0eb2e7a5f0
Thilo Molitor <thilo@eightysoft.de>
2024-04-18 22:31:33 +0200

    [MMCA] Use autogenerated main.py as proxy to real MMCA

--- src/MobileCrashAnalyzer.py
+++ src/MobileCrashAnalyzer.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
-
 import os
-import argparse
 
 from shared.utils import Paths
-
-# Prevent kivy from logging as well
-os.environ["KIVY_NO_CONSOLELOG"] = "1"
-
-# parse commandline
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description="Monal Mobile Crash Analyzer")
-parser.add_argument("--log", metavar='LOGLEVEL', help="Loglevel to log", default="DEBUG")
-args = parser.parse_args()
 
 Paths.set_personality(__file__)
 os.makedirs(Paths.user_data_dir(), exist_ok=True)
@@ -19,7 +9,6 @@ os.makedirs(Paths.user_log_dir(), exist_ok=True)
 
 # Configure logger
 import json, logging, logging.config
-
 try:
     with open(Paths.get_conf_filepath("logger.json"), 'r') as logging_configuration_file:
         logger_config = json.load(logging_configuration_file)
@@ -28,8 +17,6 @@ except:
         logger_config = json.load(fp)
     with open(Paths.get_conf_filepath("logger.json"), 'w+') as fp:
         json.dump(logger_config, fp)
-
-logger_config["handlers"]["stderr"]["level"] = args.log
 logging.config.dictConfig(logger_config)
 logger = logging.getLogger(__name__)
 logger.info('Logger configured...')
