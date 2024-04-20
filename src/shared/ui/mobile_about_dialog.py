b36a8339a85391d6a6e5f24e2327a046f5cf2b1d
Thilo Molitor <thilo@eightysoft.de>
2024-04-20 05:34:11 +0200

    [MMCA] Fix various ui bugs in mobile about dialog

--- src/shared/ui/mobile_about_dialog.py
+++ src/shared/ui/mobile_about_dialog.py
@@ -3,39 +3,50 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.gridlayout import GridLayout
 
-from shared.utils import Paths
+from jnius import autoclass, cast
+from android import activity, mActivity
+J_Intent = autoclass("android.content.Intent")
+J_Uri = autoclass("android.net.Uri")
 
+from shared.utils import Paths
 from shared.utils.version import VERSION
 
 import logging
 logger = logging.getLogger(__name__)
 
 def createResponsiveLabel(text):
-    label = Label(text=str(text), size_hint=(1, None))
+    label = Label(text=str(text), markup=True, size_hint=(1, None))
     label.bind(
         width=lambda *x:
         label.setter("text_size")(label, (label.width, None)),
-        texture_size=lambda *x: label.setter("height")(label, label.texture_size[1])
-        )
+        texture_size = lambda *x: label.setter("height")(label, label.texture_size[1])
+    )
+    def ref_pressed(instance, value):
+        logger.info(f"Reference pressed on {instance}: {value}")
+        browserIntent = J_Intent(J_Intent.ACTION_VIEW, J_Uri.parse(value));
+        mActivity.startActivity(browserIntent);
+    label.bind(on_ref_press = ref_pressed)
     return label
 
 def MobileAboutDialog(*args):
-    logger.debug("Create about popup dialog...")
+    logger.debug("Showing about popup dialog...")
 
     uiButton_close = Button(text = "Close")
-    uiGridLayout_buttons = GridLayout(cols=2, size_hint=(None, None))
+    uiGridLayout_buttons = GridLayout(cols=1, size_hint=(1, None))
     uiGridLayout_buttons.add_widget(uiButton_close)
 
     uiGridLayout_about = GridLayout(cols=1)
-    uiGridLayout_about.add_widget(createResponsiveLabel("Copyright 2023 monal-im"))
-    uiGridLayout_about.add_widget(createResponsiveLabel("Project Homepage: https://github.com/monal-im/DebugTools"))
-    uiGridLayout_about.add_widget(createResponsiveLabel("Umbrella Project Homepage: https://github.com/monal-im/Monal"))
-    uiGridLayout_about.add_widget(createResponsiveLabel("App Icon by Ann-Sophie Zwahlen: https://art.of-sophy.ch/"))
-    uiGridLayout_about.add_widget(createResponsiveLabel(Paths.user_data_dir()))
-    uiGridLayout_about.add_widget(createResponsiveLabel(Paths.user_log_dir()))
+    uiGridLayout_about.add_widget(createResponsiveLabel("Copyright 2024 monal-im"))
+    uiGridLayout_about.add_widget(createResponsiveLabel("Project Homepage: [ref=https://github.com/monal-im/DebugTools][color=3391ff]https://github.com/monal-im/DebugTools[/color][/ref]"))
+    uiGridLayout_about.add_widget(createResponsiveLabel("Umbrella Project Homepage: [ref=https://github.com/monal-im/Monal][color=3391ff]https://github.com/monal-im/Monal[/color][/ref]"))
+    uiGridLayout_about.add_widget(createResponsiveLabel(" "))
+    uiGridLayout_about.add_widget(createResponsiveLabel("App Icon by Ann-Sophie Zwahlen: [ref=https://art.of-sophy.ch/][color=3391ff]https://art.of-sophy.ch/[/color][/ref]"))
+    uiGridLayout_about.add_widget(createResponsiveLabel(" "))
+    uiGridLayout_about.add_widget(createResponsiveLabel(f"Version: {VERSION}"))
+    uiGridLayout_about.add_widget(createResponsiveLabel(f"Config dir: {Paths.user_data_dir()}"))
+    uiGridLayout_about.add_widget(createResponsiveLabel(" "))
     uiGridLayout_about.add_widget(uiGridLayout_buttons)
 
-    popup = Popup(title="About", content = uiGridLayout_about)   
-    popup.open()    
-
-    uiButton_close.bind(on_press = popup.dismiss) 
+    popup = Popup(title="About", content = uiGridLayout_about)
+    uiButton_close.bind(on_press = popup.dismiss)
+    popup.open()
