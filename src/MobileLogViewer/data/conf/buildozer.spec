8eeb22c0496734aa61244bb264e849caa38b318a
Sobottasgithub <Sobotta2008@outlook.de>
2024-04-25 17:06:57 +0200

    fix: Release an apk for the mobile log viewer as well, fixes #19

---
+++ src/MobileLogViewer/data/conf/buildozer.spec
@@ -0,0 +1,30 @@
+[buildozer]
+log_level = 2
+
+[app]
+title = Mobile Log Viewer
+package.name = MobileLogViewer
+package.domain = im.monal
+
+source.dir = .
+source.include_exts = py,png,jpg,kv,atlas,json
+source.include_patterns = shared/*,MobileLogViewer/*
+icon.filename = %(source.dir)s/MobileLogViewer/data/art/icon.png
+presplash.filename = %(source.dir)s/MobileLogViewer/data/art/icon.png
+
+requirements = python3,kivy,platformdirs,logging,jnius
+
+orientation = portrait
+fullscreen = 0
+
+# android specific
+android.minapi = 29
+android.permissions = WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE
+android.arch = armeabi-v7a
+android.manifest.intent_filters = intent_filters.xml
+
+# iOS specific
+ios.kivy_ios_url = https://github.com/kivy/kivy-ios
+ios.kivy_ios_branch = master
+ios.ios_deploy_url = https://github.com/phonegap/ios-deploy
+ios.ios_deploy_branch = 1.7.0
--- src/MobileLogViewer/ui/main_window.py
+++ src/MobileLogViewer/ui/main_window.py
@@ -3,14 +3,27 @@ from kivy.app import App
 
 from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.scrollview import ScrollView
+from kivy.clock import mainthread
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.factory import Factory
+from kivy.utils import platform
 
 import functools
+import os
 from collections import defaultdict
+from jnius import autoclass, cast
+
+# Just import if the os is Android to avoid Android peculiarities
+if platform == "android":
+    from android import activity, mActivity, permissions
+    J_FileOutputStream = autoclass("java.io.FileOutputStream")
+    J_FileUtils = autoclass("android.os.FileUtils")
+    #J_Intent = autoclass("android.content.Intent")
+    #J_PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    permissions.request_permissions([permissions.Permission.READ_EXTERNAL_STORAGE, permissions.Permission.WRITE_EXTERNAL_STORAGE])
 
 from shared.utils.constants import LOGLEVELS
 from shared.storage import Rawlog
@@ -72,10 +85,76 @@ class MainWindow(App):
         self.layout.add_widget(gridLayout_menueBar)
         self.layout.add_widget(self.uiScrollWidget_logs)
 
+        # Just import if the os is Android to avoid Android peculiarities
+        if platform == "android":
+            activity.bind(on_new_intent=self.on_new_intent)
+
         return self.layout
         
     def quit(self, *args):
         self.stop()
+
+    def on_start(self, *args):
+        # Just import if the os is Android to avoid Android peculiarities
+        if platform == "android":
+            context = cast('android.content.Context', mActivity.getApplicationContext())
+            logger.info(f"Startup application context: {context}")
+            intent = mActivity.getIntent()
+            logger.info(f"Got startup intent: {intent}")
+            if intent:
+                self.on_new_intent(intent)
+    
+    #see https://github.com/termux/termux-app/blob/74b23cb2096652601050d0f4951f9fb92577743c/app/src/main/java/com/termux/filepicker/TermuxFileReceiverActivity.java#L70
+    @mainthread
+    def on_new_intent(self, intent):
+        logger.info("Got new intent with action: %s" % str(intent.getAction()))
+        logger.debug("Raw intent: %s" % str(intent))
+        if intent.getAction() == "android.intent.action.VIEW":
+            logger.info("Intent scheme: %s" % intent.getScheme())
+            logger.info("Intent type: %s" % intent.getType())
+            logger.info("Intent data: %s" % intent.getData())
+            logger.info("Intent path: %s" % intent.getData().getPath())
+            
+            uri = intent.getData()
+            context = mActivity.getApplicationContext()
+            contentResolver = context.getContentResolver()
+            
+            cacheFile = Paths.get_cache_filepath("intent.file")
+            if os.path.exists(cacheFile):
+                os.remove(cacheFile)
+            logger.debug(f"Writing file at '{uri.getPath()}' to '{cacheFile}'...")
+            bytecount = J_FileUtils.copy(contentResolver.openInputStream(uri), J_FileOutputStream(cacheFile))
+            logger.debug(f"{bytecount} bytes copied...")
+            self.openFile(cacheFile)
+            os.remove(cacheFile)
+            
+            """
+            logger.info("Intent uri: %s" % intent.getParcelableExtra(J_Intent.EXTRA_STREAM))
+            logger.info("Intent text: %s" % intent.getStringExtra(J_Intent.EXTRA_TEXT))
+            uri = intent.getParcelableExtra(J_Intent.EXTRA_STREAM)
+            context = mActivity.getApplicationContext()
+            contentResolver = context.getContentResolver()
+            if uri != None and type(uri) != str:
+                logger.info("Real android.net.Uri found...")
+                uri = cast("android.net.Uri", uri)
+                if uri.getScheme().lower() != 'content':
+                    logger.error("Uri scheme not supported: '%s'" % uri.getScheme())
+                    return
+                cacheFile = Paths.get_cache_filepath("intent.file")
+                if os.path.exists(cacheFile):
+                    os.remove(cacheFile)
+                J_FileUtils.copy(contentResolver.openInputStream(uri), J_FileOutputStream(cacheFile))
+                self.openFile(cacheFile)
+                os.remove(cacheFile)
+            else:
+                logger.info("Str based uri found...")
+                cacheFile = Paths.get_cache_filepath("intent.file")
+                if os.path.exists(cacheFile):
+                    os.remove(cacheFile)
+                J_FileUtils.copy(contentResolver.openInputStream(intent.getData()), J_FileOutputStream(cacheFile))
+                self.openFile(cacheFile)
+                os.remove(cacheFile)
+            """
     
     def selectFile(self, *args):
         # Create popup window to select file
