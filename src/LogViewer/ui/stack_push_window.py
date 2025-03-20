5d322639625a2352100abe2a571603f034e50ab3
Sobottasgithub <sobotta2008@outlook.de>
2025-03-20 14:44:52 +0100

    [MLV] Prefill stack entry name with incrmenting names like "Entry #1", "Entry #2" etc, fixes #61

--- src/LogViewer/ui/stack_push_window.py
+++ src/LogViewer/ui/stack_push_window.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 @UiAutoloader
 class StackPushWindow(QtWidgets.QDialog):
     def __init__(self, names):
+        self.uiLineEdit_name.setText(self.generateUniqueName(names))
         self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).clicked.connect(self.accept)
         self.names = names
 
@@ -28,3 +29,14 @@ class StackPushWindow(QtWidgets.QDialog):
     @catch_exceptions(logger=logger)
     def getName(self):
         return self.name
+    
+    def generateUniqueName(self, names):
+        presentStacks = [entry for entry in names if entry.startswith("Stack #")]
+
+        uniqueId = 1
+        uniqueName = f"Stack #{uniqueId}"
+        while uniqueName in names:
+            uniqueId += 1
+            uniqueName = f"Stack #{uniqueId}"
+
+        return uniqueName
